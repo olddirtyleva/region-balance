0fcec745cb833d6979e3f193b881a7e2359a813c
olddirtyleva <Levzonin@mail.ru>
2025-01-18 15:48:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -810,4 +810,4 @@ def analyze_excel_files(base_filepath, current_filepath):
 
 if __name__ == '__main__':
   from waitress import serve
-  serve(app, host="0.0.0.0", port=8080)
+  serve(app, host="0.0.0.0", port=8888)
