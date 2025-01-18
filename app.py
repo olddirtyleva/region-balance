ee03f41d4ef85b8743534067044a90e10192e967
Lev <Levzonin@mail.ru>
2025-01-18 12:56:45 +0300

    test wsgi

--- app.py
+++ app.py
@@ -809,4 +809,5 @@ def analyze_excel_files(base_filepath, current_filepath):
   return full_content
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  from waitress import serve
+  serve(app, host="0.0.0.0", port=8080)
